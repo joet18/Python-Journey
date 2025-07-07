791ad15864ffbfd441e0b3c00a1854bab32eaafb
Joet <j.lil.boy116@gmail.com>
2025-07-07 18:31:46 +0300

    1st Python Project

---
+++ 1st.py
@@ -0,0 +1,23 @@
+#first project#
+adj1 = "great"
+verb = "coding"
+time = 3
+
+print(f"I am a {adj1} Programmer and I love {verb} Python!")
+        
+print(f"Can you call me at {time} o'clock?")
+#2nd project create an are calculator#
+
+length=float(input("Enter the length:"))
+width=float(input("Enter the width:"))
+area=length*width
+print(f"The area of the rectangle is {round(area, 2)}")
+
+#The 3rd project Calculate the price of items#
+
+item=input("Enter the item name:")
+price=float(input("Enter the price of the item:"))
+quantity=int(input("Enter the quantity of the item:"))
+total_price=price*quantity
+print(f"The total ordered items is {quantity} {item}(s) at a price of {price} each, making the total price {round(total_price, 2)}.")
+
